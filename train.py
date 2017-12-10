27b6bb260653bb7f9379f9918bcecbe905e8026d
JinChengneng <1024882812@qq.com>
2017-12-10 13:40:37 +0800

    finish feature extraction

--- train.py
+++ train.py
@@ -3,23 +3,35 @@ import matplotlib.image as mpimg #mpimg 用于读取图片
 import numpy as np
 from scipy import misc  #对图像进行缩放
 
+from feature import NPDFeature
 #%matplotlib inline
 
-datasetSize = 300
-img_face = []
-img_nonface = []
+sampleSize = 400
+#img_face = []
+#img_nonface = []
+
+img =[]
+img_label = []
+img_features = []
+img_label_train, img_label_validation = [],[]
+img_features_train, img_features_validation = [],[]
 
 def load_img_data():
-    for i in range(0,datasetSize):
+    # img_face
+    for i in range(0,int(sampleSize/2)):
         image = mpimg.imread("./datasets/original/face/face_"+"{:0>3d}".format(i)+".jpg")
         image_gray = rgb2gray(image)
         image_gray_scaled = misc.imresize(image_gray,(24,24))
-        img_face.append(image_gray_scaled)
-    for i in range(0,datasetSize):
+        img.append(image_gray_scaled)
+        img_label.append(1)
+#    #img_nonface
+#    for i in range(0,int(sampleSize/2)):
         image = mpimg.imread("./datasets/original/nonface/nonface_"+"{:0>3d}".format(i)+".jpg")
         image_gray = rgb2gray(image)
         image_gray_scaled = misc.imresize(image_gray,(24,24))
-        img_nonface.append(image_gray_scaled)
+        img.append(image_gray_scaled)
+        img_label.append(-1)
+   
 
 def showImg(item):
     plt.imshow(item)
@@ -27,7 +39,18 @@ def showImg(item):
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
+def extra_img_features():
+    for i in range(0,len(img)):
+        f = NPDFeature(img[i])
+        features = f.extract()
+        img_features.append(features)
+
 if __name__ == "__main__":
     load_img_data()
-    showImg(img_nonface[3])
+    extra_img_features()
+    img_label_train = img_label[0:int(sampleSize*0.7)]
+    img_label_validation = img_label[int(sampleSize*0.7):]
+    img_features_train = img_features[0:int(sampleSize*0.7)]
+    img_features_validation = img_features[int(sampleSize*0.7):]
+
 
