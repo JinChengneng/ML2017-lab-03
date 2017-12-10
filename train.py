37ab769d84b4898c14bd8a810e97df389d592d4d
JinChengneng <1024882812@qq.com>
2017-12-10 11:38:07 +0800

    finish load_image_data function

--- train.py
+++ train.py
@@ -1,6 +1,33 @@
+import matplotlib.pyplot as plt #plt 用于显示图片
+import matplotlib.image as mpimg #mpimg 用于读取图片
+import numpy as np
+from scipy import misc  #对图像进行缩放
 
+#%matplotlib inline
+
+datasetSize = 300
+img_face = []
+img_nonface = []
+
+def load_img_data():
+    for i in range(0,datasetSize):
+        image = mpimg.imread("./datasets/original/face/face_"+"{:0>3d}".format(i)+".jpg")
+        image_gray = rgb2gray(image)
+        image_gray_scaled = misc.imresize(image_gray,(24,24))
+        img_face.append(image_gray_scaled)
+    for i in range(0,datasetSize):
+        image = mpimg.imread("./datasets/original/nonface/nonface_"+"{:0>3d}".format(i)+".jpg")
+        image_gray = rgb2gray(image)
+        image_gray_scaled = misc.imresize(image_gray,(24,24))
+        img_nonface.append(image_gray_scaled)
+
+def showImg(item):
+    plt.imshow(item)
+    
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    load_img_data()
+    showImg(img_nonface[3])
 
